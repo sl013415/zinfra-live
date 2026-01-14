ac097509e29ede97ea5fae72151e6508d75dd5e7
sl013415 <118207567+sl013415@users.noreply.github.com>
2026-01-14 21:04:34 +0530

    Add files via upload

---
+++ Price-MGMTGroup-2/Zelis-Price-Development/zcmssrgq/sharedservices/providers.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    azurerm = ">=3.15.1, <4.0.0"
+    
+  }
+  backend  "azurerm" {
+    subscription_id      = "5129001f-5fc6-4a22-9e42-70ef99374bd4"
+    resource_group_name  = "zentfrg"
+    storage_account_name = "zenstrtfstatep01"
+    container_name       = "tfstate"
+    key                  = "Price-MGMTGroup/Zelis-Price-Development/zcmssrgq/SharedServices/terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+provider "azurerm" {
+  alias="certkvtsub"
+  features {}
+  subscription_id = "5129001f-5fc6-4a22-9e42-70ef99374bd4"
+}
+ provider "aws" {
+   region = "us-east-2"
+ }
